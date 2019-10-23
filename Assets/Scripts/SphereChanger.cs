d2b3f96665abd323bd4c684eefe9bfbcc5faa7e1
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2019-10-22 23:49:27 -0500

    Update sphere hotspots

--- Assets/Scripts/SphereChanger.cs
+++ Assets/Scripts/SphereChanger.cs
@@ -9,7 +9,9 @@ using UnityEngine.Video;
 public class SphereChanger : MonoBehaviour
 {
     public GameObject m_Fader;
-    VideoPlayer currentSphere;
+    GameObject nextSphere;
+    VideoPlayer currentSphereVideo;
+    VideoPlayer nextSphereVideo;
     // Animator faderAnimator;
     //This ensures that we don't mash to change spheres
     // bool changing = false;
@@ -29,17 +31,14 @@ public class SphereChanger : MonoBehaviour
     }
     IEnumerator FadeCamera(GameObject nextSphere)
     {
-        currentSphere = gameObject.GetComponentInParent<VideoPlayer>();
 
-        currentSphere.Stop();
-        currentSphere.isActiveAndEnabled
         //Ensure we have a fader object
         if (m_Fader != null)
         {
             //Fade the Quad object in and wait 0.75 seconds
-
-            StartCoroutine(FadeIn(0.75f, m_Fader.GetComponent<Renderer>().material));
+            StartCoroutine(FadeIn(0.75f, m_Fader.GetComponent<Renderer>().material, nextSphere));
             yield return new WaitForSeconds(0.75f);
+
 
             //Change the camera position
             Camera.main.transform.parent.position = nextSphere.transform.position;
@@ -47,6 +46,7 @@ public class SphereChanger : MonoBehaviour
             //Fade the Quad object out 
             StartCoroutine(FadeOut(0.75f, m_Fader.GetComponent<Renderer>().material));
             yield return new WaitForSeconds(0.75f);
+
         }
         else
         {
@@ -57,21 +57,12 @@ public class SphereChanger : MonoBehaviour
     }
 
 
-    IEnumerator FadeOut(float time, Material mat)
+    IEnumerator FadeIn(float time, Material mat, GameObject sphere)
     {
-        //While we are still visible, remove some of the alpha colour
-        while (mat.color.a >= 0.0f)
-        {
-            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a - (Time.deltaTime / time));
-            //change the bool to true when its completely fade out                      
-            yield return null;
-        }
-    }
-
-
-    IEnumerator FadeIn(float time, Material mat)
-    {
-        //While we aren't fully visible, add some of the alpha colour
+        // nextSphereVideo = nextSphere.GetComponent<VideoPlayer>();
+        // nextSphereVideo.enabled = true;
+        // nextSphereVideo.Play();
+        // //While we aren't fully visible, add some of the alpha colour
         while (mat.color.a <= 1.0f)
         {
             mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a + (Time.deltaTime / time));
@@ -80,6 +71,23 @@ public class SphereChanger : MonoBehaviour
         }
 
     }
+    IEnumerator FadeOut(float time, Material mat)
+    {
+        currentSphereVideo = gameObject.GetComponentInParent<VideoPlayer>();
+        Debug.Log(GetComponentInParent<VideoPlayer>());
+        // Stop current video, start next video
+        //While we are still visible, remove some of the alpha colour
+        while (mat.color.a >= 0.0f)
+        {
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a - (Time.deltaTime / time));
+            //change the bool to true when its completely fade out                      
+            yield return null;
+        }
+        //     currentSphereVideo.Stop();
+        //     currentSphereVideo.enabled = false;
+    }
+
+
 
 
 }
